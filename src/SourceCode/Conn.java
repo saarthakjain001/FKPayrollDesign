a22b3f5e317d2d1682205d45cdba5f613759b1e6
Saarthak Jain <f20170083@hyderabad.bits-pilani.ac.in>
2020-05-09 23:42:19 +0530

    For feature adding employee: 	1.Created Connection class with methods for starting and closing connection with database. 	2.Created Employee class and its children classes(Only the attributes)

---
+++ src/SourceCode/Conn.java
@@ -0,0 +1,71 @@
+package SourceCode;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.*;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author stark001
+ */
+
+
+public class Conn {
+    static Connection con;
+    
+
+    static Conn instance = null;
+
+
+    public static void setConn() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("driver registered");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql", "root", "root");
+            System.out.println("connection made");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public static void closeConn(){
+        try{
+            con.close();
+            System.out.println("Conn Closed");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Could not close connection");
+        }
+    }
+    
+    public static ResultSet executeQuery(String query){
+         
+        ResultSet rs = null;
+        Statement statement;
+         try
+         {
+             statement= con.createStatement();
+             rs = statement.executeQuery(query); 
+         } 
+         catch(SQLException e)
+         {
+             System.out.println("Statement cannot be created");
+         }
+         finally{
+             return rs;
+         }
+    
+    }
+    
+    public static void main(String[] args) {
+ setConn();
+
+ }
+}
---
+++ src/SourceCode/Employee.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SourceCode;
+import java.util.Date;
+/**
+ *
+ * @author stark001
+ */
+public class Employee {
+    private String name,category,modeOfPayment,address,bankDetails;
+    private Date lastPaymentDate; 
+    private int commissionRate,id,charges,totalRecievedAmount;
+    
+    public Employee(){
+        
+    }
+      
+}
+//public class HourlyEmployee extends Employee{
+//    private int hourlyRate;
+//    
+//}
+//public class SalariedEmployee extends Employee{
+//   private int salary;
+//}
